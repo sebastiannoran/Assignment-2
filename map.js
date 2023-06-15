ea7fb0a79c57d95c4827434d162dbdb2c8ba4500
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-15 14:53:51 -0400

    explaniation only

--- map.js
+++ map.js
@@ -2,4 +2,14 @@
 // compose a function titled “myMap” that will take in an array of elements 
 // and executes a callback function (provided by you) on each of those elements.
 
+//map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): U[];
+    /**
+     * Returns the elements of an array that meet the condition specified in a callback function.
+     * @param predicate A function that accepts up to three arguments. The filter method calls 
+     * the predicate function one time for each element in the array.
+     * @param thisArg An object to which the this keyword can refer in the predicate function. 
+     * If thisArg is omitted, undefined is used as the this value.
+     */
+
 const jobTitles = ["Engineer", "Product Manager", "Director", "Nurse"];
+
