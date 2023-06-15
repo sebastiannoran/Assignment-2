0de1ae218912600474d8740da446853aeb9093d7
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-15 14:30:06 -0400

    Initilal Commit to Main

---
+++ forEach.js
@@ -0,0 +1,35 @@
+// We highly suggest utilizing Mozilla Developer Network (MDN) Web Docs to understand 
+//how each method works and what arguments they take. Also look into Array.prototype.pop(), 
+//Array.prototype.shift(), and other methods for your own enrichment.
+
+//Without using the native “Array.prototype.forEach” method of JavaScript, 
+//compose a function titled “myEach” that will take in an array of elements 
+//and executes any callback function (provided by you) on each of those elements.
+
+    /*
+     *forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void;
+     * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+     * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+     * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+     */
+
+const jobTitles = ["Engineer", "Product Manager", "Director", "Nurse"];
+const secondTitles = [];
+
+//jobTitles.forEach(element => console.log(element));
+
+let myCallBack = function (arr) {
+    for (i = 0; i < arr.length; i++) {
+        console.log(secondTitles.push(arr[i]))
+    }
+}
+
+let myEach = function (arr) {
+    return myCallBack(arr);
+ }
+
+myEach(jobTitles);
+
+
+
+
---
+++ map.js
@@ -0,0 +1,5 @@
+// Without using the native “Array.prototype.map” method of JavaScript, 
+// compose a function titled “myMap” that will take in an array of elements 
+// and executes a callback function (provided by you) on each of those elements.
+
+const jobTitles = ["Engineer", "Product Manager", "Director", "Nurse"];
