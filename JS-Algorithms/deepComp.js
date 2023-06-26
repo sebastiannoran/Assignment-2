93042b92faa14453c69112cfe5929fd0a0fbc903
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-25 22:03:00 -0400

    deep comp complete

--- JS-Algorithms/deepComp.js
+++ JS-Algorithms/deepComp.js
@@ -5,15 +5,39 @@
 // with a recursive call to deepEqual.
 
 // function will return a boolean value to indicate the above
-// use the hints to guide the answer
+
+// let deepEqual = function (obj1, obj2) {
+// if (typeof(obj1) === typeof(obj2)) {
+//     return true;
+// } else {
+//     return false;
+// }
+// } --> not good enough, no shallow copy
 
 let deepEqual = function (obj1, obj2) {
-if (typeof(obj1) === typeof(obj2)) {
-    return true;
-} else {
-    return false;
+    if (typeof(obj1) !== typeof(obj2)) {
+        return false;
+    }
+    if (typeof(obj1) === 'array') {
+    if (obj1.length !== obj2.length) {
+        return false; }
+    return obj1.every((element, index) => deepEqual(element, obj2[index]));
+    // use every method to iterate through elements of obj1 and check for
+    // corresponding element in the index's of obj2
+    // next, check of object datatypes
+    } else if (typeof(obj1) === "object") {
+
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+        return false;
+    // elimate based on length
+    }
+    return Object.keys(obj1).every((key) => deepEqual(obj1[key], obj2[key]));
+    // check for corresponding elements with every and keys JS functions
 }
+return obj1 === obj2;
 }
+
+
 
 let obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
@@ -22,3 +46,5 @@ console.log(deepEqual(obj, obj));
 console.log(deepEqual(obj, {here: 1, object: 2}));
 // should return false
 // @ 8:55pm, function does not work
+
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
